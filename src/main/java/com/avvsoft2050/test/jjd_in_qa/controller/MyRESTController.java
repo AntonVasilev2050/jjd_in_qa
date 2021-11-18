38417c3a7c875164f048918ff90da5f45c66242a
Anton2050 <avvsoft2050@gmail.com>
2021-11-18 23:45:03 +0300

    add JwtProvider.java

--- src/main/java/com/avvsoft2050/test/jjd_in_qa/controller/MyRESTController.java
+++ src/main/java/com/avvsoft2050/test/jjd_in_qa/controller/MyRESTController.java
@@ -1,8 +1,10 @@
 package com.avvsoft2050.test.jjd_in_qa.controller;
 
 import com.avvsoft2050.test.jjd_in_qa.entity.User;
+import com.avvsoft2050.test.jjd_in_qa.service.JwtProvider;
 import com.avvsoft2050.test.jjd_in_qa.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,9 +16,16 @@ public class MyRESTController {
     @Autowired
     private UserService userService;
 
-    public String createToken(@RequestBody User user){
+    @Autowired
+    private JwtProvider jwtProvider;
 
-
+    @PostMapping("/users/get-token")
+    public String createTokenForUser(@RequestBody User user) {
+        User userFromDB = userService.getUser(user.getUserName(), user.getPassword());
+        if(userFromDB == null){
+            return null;
+        }
+        jwtProvider.generateToken(userFromDB.getUserName());
 
         return null;
     }
---
+++ src/main/java/com/avvsoft2050/test/jjd_in_qa/service/JwtProvider.java
@@ -0,0 +1,52 @@
+package com.avvsoft2050.test.jjd_in_qa.service;
+// Courtesy of https://java-master.com/spring-security-%D1%81-%D0%BF%D0%BE%D0%BC%D0%BE%D1%89%D1%8C%D1%8E-jwt-%D1%82%D0%BE%D0%BA%D0%B5%D0%BD%D0%B0/
+
+import io.jsonwebtoken.*;
+import lombok.extern.java.Log;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+
+@Component
+@Log
+public class JwtProvider {
+
+    @Value("$(jwt.secret)")
+    private String jwtSecret;
+
+    public String generateToken(String login) {
+        Date date = Date.from(LocalDate.now().plusDays(15).atStartOfDay(ZoneId.systemDefault()).toInstant());
+        return Jwts.builder()
+                .setSubject(login)
+                .setExpiration(date)
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .compact();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token);
+            return true;
+        } catch (ExpiredJwtException expEx) {
+            log.severe("Token expired");
+        } catch (UnsupportedJwtException unsEx) {
+            log.severe("Unsupported jwt");
+        } catch (MalformedJwtException mjEx) {
+            log.severe("Malformed jwt");
+        } catch (SignatureException sEx) {
+            log.severe("Invalid signature");
+        } catch (Exception e) {
+            log.severe("invalid token");
+        }
+        return false;
+    }
+
+    public String getLoginFromToken(String token) {
+        Claims claims = Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody();
+        return claims.getSubject();
+    }
+
+}
